0edb324c77706e4bf4c8181a7cdee02fdb4bb389
nemo <nemored@gmail.com>
2022-04-01 10:35:21 -0700

    Adds support for connecting and staying connected to Mumble
    Known Issues:
    - PIPO does not appear to receive all expected messages from the
    server. It is unknown if either the socket queue is not being
    processed correctly or the messages are not being sent from the server
    in the first place.
    - Scheduling may be sub-optimal: Writing to the socket is not
    scheduled in the main tokio::select! block, so any writes (currently,
    only the two initial Version and Authentication messages sent when
    connecting and the Ping message sent every 10 seconds) will
    effectively block the rest of the Mumble task.

---
+++ src/mumble/cert_verifier.rs
@@ -0,0 +1,33 @@
+use std::time::SystemTime;
+
+use rustls::{client::{ServerCertVerifier, ServerCertVerified}, ServerName, Certificate, Error};
+
+pub struct MumbleCertVerifier {
+    cert: Certificate
+}
+
+impl MumbleCertVerifier {
+    pub fn new(cert: Certificate) -> Self {
+        Self { cert }
+    }
+}
+
+impl ServerCertVerifier for MumbleCertVerifier {
+    fn verify_server_cert(&self,
+                          end_entity: &Certificate,
+                          intermediates: &[Certificate],
+                          server_name: &ServerName,
+                          scts: &mut dyn Iterator<Item = &[u8]>,
+                          ocsp_response: &[u8],
+                          now: SystemTime)
+                          -> Result<ServerCertVerified, Error> {
+        if *end_entity == self.cert {
+            Ok(ServerCertVerified::assertion())
+        }
+        else {
+            let error = String::from("invalid peer certificate: does not match certificate loaded from server_cert file");
+
+            Err(Error::InvalidCertificateData(error))
+        }
+    }
+}
